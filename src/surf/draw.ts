6e11d49a99d3057fb6abfbf9620b1c28b5bc80b9
Dmitrii Gaidarji <dmitriigaidarji@gmail.com>
2024-11-10 18:44:09 +0800

    revert

--- src/surf/draw.ts
+++ src/surf/draw.ts
@@ -1,14 +1,11 @@
-import { learnedSummary, openai } from "./ai.ts";
+import { openai } from "./ai.ts";
 
 export async function generateImage(
   prompt: string,
 ): Promise<string | undefined> {
-  const summary = await learnedSummary().then((t) => t.content);
   const response = await openai.images.generate({
     model: "dall-e-3",
-    prompt: `Context: ${summary}
-    Actual prompt for the image is below in format {sender}: {prompt}.
-    ${prompt}`,
+    prompt,
     n: 1,
     size: "1024x1024",
   });
