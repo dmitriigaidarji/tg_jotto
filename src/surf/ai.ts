2b4695170ca6cf8f67eda1819314d2ed819d55ae
Dmitrii Gaidarji <dmitriigaidarji@gmail.com>
2025-03-30 01:46:20 +0800

    debug

--- src/surf/ai.ts
+++ src/surf/ai.ts
@@ -89,6 +89,7 @@ export async function askAI({
     .concat(await userSystemSettings())
     .concat([convertUserMessage(lastMessages)])
     .concat(messages);
+  console.log(allMessages);
   const response = await openai.chat.completions.create({
     model,
     messages: allMessages,
